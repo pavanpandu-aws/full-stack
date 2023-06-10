af3154e2e7406d2d07ff275ba21efbcfde6554fe
Mohan Jaji <pavanmohan.ce@gmail.com>
2023-06-10 13:24:02 +0530

    updated

--- backend/src/main/java/com/kirilanastasoff/TheMovieDB/backend/model/User.java
+++ backend/src/main/java/com/kirilanastasoff/TheMovieDB/backend/model/User.java
@@ -40,7 +40,7 @@ public class User {
 	private String password;
 
 	@ManyToMany(fetch = FetchType.LAZY)
-	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "role_id"))
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet();
 
 	public User() {
